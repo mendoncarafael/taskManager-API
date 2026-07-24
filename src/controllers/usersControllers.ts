041e72f49500cdb81ce824371a0cd66b27aea359
Rafael Mendonca <mendocarafael23@gmail.com>
2026-07-24 16:31:21 -0300

    refactor: improve user controller methods for better readability and error handling
    - Updated user creation logic to ensure email uniqueness check is more robust.
    - Enhanced response structure by omitting password in user responses.
    - Refactored index and indexByID methods for clarity and consistency.
    - Improved update method to prevent email conflicts when updating user details.

--- src/controllers/usersControllers.ts
+++ src/controllers/usersControllers.ts
@@ -1,94 +1,96 @@
 import { Request, Response } from "express";
-import { z } from "zod"
+import { check, z } from "zod";
 import { hash } from "bcrypt";
 import { prisma } from "../../prisma/database/prisma";
 import { AppError } from "@/utils/AppError";
 
-
 export class UsersControllers {
-    async create(request: Request, response: Response) {
-        const bodySchema = z.object({
-            name: z.string().trim().min(2).max(100),
-            email: z.string().email().trim().min(4).max(150),
-            password: z.string().min(2)
-        })
+  async create(request: Request, response: Response) {
+    const bodySchema = z.object({
+      name: z.string().trim().min(2).max(100),
+      email: z.string().email().trim().min(4).max(150),
+      password: z.string().min(2),
+    });
 
-        const {name, email, password} = bodySchema.parse(request.body)
-      
-        const checkEmail = await prisma.users.findUnique({where: {email}})
-        
-        if (checkEmail) {
-            throw new AppError("This email already registered")
-        }
-        
-        const hashedPassword = await hash(password, 8);
+    const { name, email, password } = bodySchema.parse(request.body);
 
-        const user = await prisma.users.create({
-            data: {
-                name,
-                email,
-                password: hashedPassword
-            }
-        })
+    const checkEmail = await prisma.users.findUnique({ where: { email } });
 
-        const { password: _, ...userWithoutPassword } = user;
-
-        return response.status(201).json(userWithoutPassword)
+    if (checkEmail) {
+      throw new AppError("This email already registered");
     }
 
-    async index(request: Request, response: Response) {
-        const users = await prisma.users.findMany({
-            omit: {
-                password: true
-            }
-        })
+    const hashedPassword = await hash(password, 8);
 
-        return response.status(200).json(users)
+    const user = await prisma.users.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    const { password: _, ...userWithoutPassword } = user;
+
+    return response.status(201).json(userWithoutPassword);
+  }
+
+  async index(request: Request, response: Response) {
+    const users = await prisma.users.findMany({
+      omit: {
+        password: true,
+      },
+    });
+
+    return response.status(200).json(users);
+  }
+
+  async indexByID(request: Request, response: Response) {
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    });
+
+    const { id } = paramsSchema.parse(request.params);
+
+    const users = await prisma.users.findMany({
+      omit: {
+        password: true,
+      },
+      where: {
+        id,
+      },
+    });
+
+    return response.status(200).json(users);
+  }
+
+  async update(request: Request, response: Response) {
+    const bodySchema = z.object({
+      id: z.string().uuid(),
+      name: z.string().trim().max(100).min(3),
+      email: z.string().email().trim().min(4).max(150).optional(),
+    });
+
+    const { id, name, email } = bodySchema.parse(request.body);
+
+    const checkEmail = await prisma.users.findFirst({ where: { email } });
+
+    if (checkEmail && checkEmail.id != id) {
+      throw new AppError("This email already registered");
     }
 
-    async indexByID(request: Request, response: Response) {
-        const paramsSchema = z.object({
-            id: z.string().uuid()
-        })
-
-        const {id} = paramsSchema.parse(request.params)
-
-        const users = await prisma.users.findMany({
-            omit: {
-                password: true
-            },
-            where: {
-                id
-            }
-        })
-
-        return response.status(200).json(users)
-    }
-
-    async update(request: Request, response: Response) {
-        const bodySchema = z.object({
-            id: z.string().uuid(),
-            name: z.string().trim().max(100).min(3),
-            email: z.string().email().trim().min(4).max(150).optional()
-        })
-
-        const {id, name, email } = bodySchema.parse(request.body)
-
-        const checkEmail = await prisma.users.findFirst({where: {email}})
-
-        if(checkEmail) {
-            throw new AppError("This email already registered")
-        }
-
-        const updateUser = await prisma.users.update({
-            where: {
-                id
-            },
-            data: {
-                name, 
-                email
-            }
-        })
-        return response.status(200).json({updateUser})
-    }
+    const updateUser = await prisma.users.update({
+      where: {
+        id,
+      },
+      omit: {
+        password: true,
+      },
+      data: {
+        name,
+        email,
+      },
+    });
+    return response.status(200).json({ updateUser });
+  }
 }
