7e28b8e7ce21e2bbfd11eeae7bc5989ca1c13ca6
˜mendocarafael˜ <mendocarafael23@gmail.com>
2026-07-30 19:42:28 -0300

    refactor: remove duplicate filter route from tasksRoutes

--- src/routes/tasksRoutes.ts
+++ src/routes/tasksRoutes.ts
@@ -7,7 +7,6 @@ const tasksController = new TasksController();
 
 tasksRoutes.post("/", tasksController.create);
 tasksRoutes.get("/filter", tasksController.filterStatus);
-tasksRoutes.get("/filter", tasksController.filterPriority);
 tasksRoutes.get("/", ensureAuthenticated, tasksController.index);
 tasksRoutes.get("/:userID", tasksController.attributedTo);
 tasksRoutes.patch("/:id/:userID/task", tasksController.assignTo);
